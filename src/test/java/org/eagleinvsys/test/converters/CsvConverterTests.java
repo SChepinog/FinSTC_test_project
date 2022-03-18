01fc0ea4f60635153e49269482cf6f8b495188f4
SChepinog <chepinoga-sv@yandex.ru>
2022-03-18 23:02:27 +0300

    Initial commit

---
+++ src/test/java/org/eagleinvsys/test/converters/CsvConverterTests.java
@@ -0,0 +1,14 @@
+package org.eagleinvsys.test.converters;
+
+import org.junit.jupiter.api.Test;
+
+class CsvConverterTests {
+
+    @Test
+    public void nanaana() {
+
+    }
+
+    // TODO: implement JUnit 5 tests for CsvConverter
+
+}
