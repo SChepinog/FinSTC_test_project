86a954feee34ba03791b5eb48b0611cb78c19dec
SChepinog <chepinoga-sv@yandex.ru>
2022-03-20 21:47:14 +0300

    Add tests for several records in csv, for null-values

--- src/test/java/org/eagleinvsys/test/converters/CsvConverterTests.java
+++ src/test/java/org/eagleinvsys/test/converters/CsvConverterTests.java
@@ -1,7 +1,11 @@
 package org.eagleinvsys.test.converters;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.eagleinvsys.test.converters.impl.CsvCollection;
 import org.eagleinvsys.test.converters.impl.CsvConverter;
@@ -30,5 +34,61 @@ class CsvConverterTests {
     public void emptyCollection() {
         csvConverter.convert(new CsvCollection(Collections.emptyList()), outputStream);
         Assertions.assertEquals("", Utils.outputStreamToString(outputStream));
+    }
+
+    @Test
+    public void oneRecordInCsv() {
+        Map<String, String> testMap = new HashMap<>();
+        testMap.put("1", "111");
+        testMap.put("2", "222");
+        testMap.put("3", "333");
+        List<Map<String, String>> data = Collections.singletonList(testMap);
+        ConvertibleCollection convertibleMessage = new CsvCollection(data);
+
+        csvConverter.convert(convertibleMessage, outputStream);
+        Assertions.assertEquals(
+            "1,2,3\n111,222,333",
+            Utils.outputStreamToString(outputStream)
+        );
+    }
+
+    @Test
+    public void severalRecords() {
+        Map<String, String> testMap1 = new HashMap<>();
+        testMap1.put("1", "111");
+        testMap1.put("2", "222");
+        testMap1.put("3", "333");
+        Map<String, String> testMap2 = new HashMap<>();
+        testMap2.put("1", "11");
+        testMap2.put("2", "22");
+        testMap2.put("3", "33");
+        List<Map<String, String>> data = Arrays.asList(testMap1, testMap2);
+        ConvertibleCollection convertibleMessage = new CsvCollection(data);
+
+        csvConverter.convert(convertibleMessage, outputStream);
+        Assertions.assertEquals(
+            "1,2,3\n111,222,333\n11,22,33",
+            Utils.outputStreamToString(outputStream)
+        );
+    }
+
+    @Test
+    public void recordsWithNull() {
+        Map<String, String> testMap1 = new HashMap<>();
+        testMap1.put("1", "111");
+        testMap1.put("2", "222");
+        testMap1.put("3", "333");
+        Map<String, String> testMap2 = new HashMap<>();
+        testMap2.put("1", "11");
+        testMap2.put("2", null);
+        testMap2.put(null, "33");
+        List<Map<String, String>> data = Arrays.asList(testMap1, testMap2);
+        ConvertibleCollection convertibleMessage = new CsvCollection(data);
+
+        csvConverter.convert(convertibleMessage, outputStream);
+        Assertions.assertEquals(
+            "null,1,2,3\nnull,111,222,333\n33,11,null,null",
+            Utils.outputStreamToString(outputStream)
+        );
     }
 }
--- src/test/java/org/eagleinvsys/test/converters/StandardCsvConverterTests.java
+++ src/test/java/org/eagleinvsys/test/converters/StandardCsvConverterTests.java
@@ -1,6 +1,7 @@
 package org.eagleinvsys.test.converters;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -42,16 +43,49 @@ class StandardCsvConverterTests {
         testMap.put("2", "222");
         testMap.put("3", "333");
         List<Map<String, String>> data = Collections.singletonList(testMap);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
         standardCsvConverter.convert(data, outputStream);
-        System.out.println(Utils.outputStreamToString(outputStream));
         Assertions.assertEquals(
             "1,2,3\n111,222,333",
             Utils.outputStreamToString(outputStream)
         );
     }
 
-//    public void
+    @Test
+    public void severalRecords() {
+        Map<String, String> testMap1 = new HashMap<>();
+        testMap1.put("1", "111");
+        testMap1.put("2", "222");
+        testMap1.put("3", "333");
+        Map<String, String> testMap2 = new HashMap<>();
+        testMap2.put("1", "11");
+        testMap2.put("2", "22");
+        testMap2.put("3", "33");
+        List<Map<String, String>> data = Arrays.asList(testMap1, testMap2);
 
-    //TODO add tests for several maps with different key set
+        standardCsvConverter.convert(data, outputStream);
+        Assertions.assertEquals(
+            "1,2,3\n111,222,333\n11,22,33",
+            Utils.outputStreamToString(outputStream)
+        );
+    }
+
+    @Test
+    public void recordsWithNull() {
+        Map<String, String> testMap1 = new HashMap<>();
+        testMap1.put("1", "111");
+        testMap1.put("2", "222");
+        testMap1.put("3", "333");
+        Map<String, String> testMap2 = new HashMap<>();
+        testMap2.put("1", "11");
+        testMap2.put("2", null);
+        testMap2.put(null, "33");
+        List<Map<String, String>> data = Arrays.asList(testMap1, testMap2);
+
+        standardCsvConverter.convert(data, outputStream);
+        Assertions.assertEquals(
+            "null,1,2,3\nnull,111,222,333\n33,11,null,null",
+            Utils.outputStreamToString(outputStream)
+        );
+    }
 }
